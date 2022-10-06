328fcb6bad3ad5967531a157d86dd7180a260a79
Daniel <20088708@wit.ie>
2022-10-06 16:38:48 +0100

    React basic labs

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     <div className="container" >
      <h1> Tasky </h1>
      <div className='desc'>
-     <Task title="Dishes" deadline="Today" description="Empty Dishwasher"/>
-     <Task title="Laundry" deadline="Tomorrow" description="Fold Laundry and put away"/> 
+     <Task title="Dishes" deadline="Today" description="Empty Dishwasher!"/>
+     <Task title="Laundry" deadline="Tomorrow" description="Fold Laundry and put away!"/> 
      <Task title="Tidy" deadline="Today" />
       </div>
     </div>
