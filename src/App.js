d28a46bd4f84ed583a4b0182cac86bbbf44f2919
Daniel <20088708@wit.ie>
2022-10-17 12:17:09 +0100

    Week 3 done

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import Task from './Task';
 import React, { useState } from 'react';
 import AddTaskForm from './Form';
 import { v4 as uuidv4 } from 'uuid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 
 function App() { 
 const [ taskState, setTaskState ] = useState({
   tasks: [
-    {id:1, title:"Dishes",description: "Empty Dishwasher", deadline:"Today", done:false},
-    {id:2, title: "Laundry", description:"Fold Clothes and put away", deadline:"Tomorrow",done:false},
-    {id:3, title:"Tidy up", deadline:"Today",done:false},
-    {id:4, title:"Hoover house", description:"Hoover the house",deadline:"Tomorrow ", done:false},
-    {id:5, title:"Mop house",description:"Mop the house", deadline:"Tomorrow",done:false}
+    {id:1, title:"Dishes",description: "Empty Dishwasher", priority:"Low", deadline:"Today", done:false},
+    {id:2, title: "Laundry", description:"Fold Clothes and put away", priority:"High",deadline:"Tomorrow",done:false},
+    {id:3, title:"Tidy up", deadline:"Today",priority:"Medium",done:false},
+    {id:4, title:"Hoover house", description:"Hoover the house",priority:"Low",deadline:"Tomorrow ", done:false},
+    {id:5, title:"Mop house",description:"Mop the house", deadline:"Tomorrow",priority:"High",done:false}
   ]
 })
 const doneHandler = (taskIndex) => {
@@ -27,8 +30,10 @@ const deleteHandler = (taskIndex) => {
 const [ formState, setFormState ] = useState({
   title: "",
   description: "",
+  priority:"",
   deadline: ""
 });
+
 const formChangeHandler = (event) => {
   let form = {...formState};
 
@@ -42,12 +47,16 @@ const formChangeHandler = (event) => {
     case "deadline":
         form.deadline = event.target.value;
         break;
+        case "priority":
+          form.priority = event.target.value;
+          break;
     default:
         form = formState;
   }
   setFormState(form);
   console.log(formState);
 }
+
 const formSubmitHandler = (event) => {
   event.preventDefault();
 
@@ -62,20 +71,54 @@ const formSubmitHandler = (event) => {
 
   return (
     <div className="container">
-      <h1>Tasky</h1>
+      <Container component="main">
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          gutterBottom
+          sx = {{
+            backgroundColor: 'gray',
+            textAlign: 'center',
+            color: 'white',
+            padding: '20px',
+            margin: '20px 0 40px 0',
+            borderRadius: '4px'
+          }}
+        >
+          Tasky
+        </Typography>
+      </Container>
 
-      {taskState.tasks.map((task, index) => (              
-    <Task 
-      title={task.title}
-      description={task.description}
-      deadline={task.deadline}
-      key={task.id}
-      done={task.done}
-      markDone={() => doneHandler(index)}
-      deleteTask = {() => deleteHandler(index)}
-    />
-  ))}
-      <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
+   <Container maxWidth="md" component="main">
+        <Grid container spacing={5} alignItems="flex-top" justifyContent="center">
+          {taskState.tasks.map((task, index) => (
+                <Task 
+                title={task.title}
+                description={task.description}
+                deadline={task.deadline}
+                priority={task.priority}
+                done={task.done}
+                key={task.id}
+                markDone = {() => doneHandler(index)}
+                deleteTask = {() => deleteHandler(index)}
+              />
+          ))}
+        </Grid>
+      </Container>
+
+      <Container
+        component="footer"
+        sx={{
+          borderTop: (theme) => `1px solid ${theme.palette.divider}`,
+          my: 6,
+          py: 6,
+        }}
+      >
+        <Grid container justifyContent="center">
+          <AddTaskForm submit={formSubmitHandler} change={formChangeHandler}/>
+        </Grid>
+      </Container>
     </div>
   );
 }
